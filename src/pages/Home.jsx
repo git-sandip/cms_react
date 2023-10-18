939c0f82d4550f37e48d36f83a548dd05728f0d0
Sandip Sapkota <sandipsapkota9844@gmail.com>
2023-10-18 20:53:20 +0545

    fixed css issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,30 +2,33 @@ import "../css/App.css";
 import Navbar from "../components/Navbar";
 import axios from "axios";
 import { useEffect, useState } from "react";
+
 const Home = () => {
-  const [products, setproducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
   const fetchProducts = async () => {
     const response = await axios.get(
       "https://652fbacd6c756603295d8edd.mockapi.io/products"
     );
-    setproducts(response.data);
+    setProducts(response.data);
   };
+
   useEffect(() => {
     fetchProducts();
   }, []);
+
   return (
     <>
       <Navbar />
-      {products.map((products) => {
-        return (
-          <div key={products.id} className="card">
-            <img src={products.productImage} alt="Product Image" />
-            <h2 className="product-name">{products.productName}</h2>
-            <p className="product-description">{products.productDescription}</p>
+      <div className="grid-container">
+        {products.map((product) => (
+          <div key={product.id} className="card">
+            <img src={product.productImage} alt="Product Image" />
+            <h2 className="product-name">{product.productName}</h2>
+            <p className="product-description">{product.productDescription}</p>
           </div>
-        );
-      })}
+        ))}
+      </div>
     </>
   );
 };
